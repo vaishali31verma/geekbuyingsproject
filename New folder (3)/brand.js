308aeadf3f55af4c20947f76c1987f9d1e676454
vaishaliverma31 <dkverma92595@gmail.com>
2022-06-16 19:11:24 +0530

    brand page

---
+++ New folder (3)/brand.js
@@ -0,0 +1,89 @@
+function logoimg() {
+    return `
+<img data-original="//img.gkbcdn.com/s3/b/2006/fiido.jpg?v=-674318602"
+    src="//img.gkbcdn.com/s3/b/2006/fiido.jpg?v=-674318602" style="display: inline;">
+<img data-original="https://img.gkbcdn.com/s3/b/Logo/tronsmart.jpg?v=788944783"
+    src="https://img.gkbcdn.com/s3/b/Logo/tronsmart.jpg?v=788944783" style="display: inline;">
+<img data-original="https://img.gkbcdn.com/s3/b/Logo/one-netbook.jpg"
+    src="https://img.gkbcdn.com/s3/b/Logo/one-netbook.jpg" style="display: inline;">
+<img data-original="https://img.gkbcdn.com/s3/b/Logo/himo.jpg?v=1500289693"
+    src="https://img.gkbcdn.com/s3/b/Logo/himo.jpg?v=1500289693" style="display: inline;">
+<img data-original="//img.gkbcdn.com/s3/b/artillery.jpg" src="//img.gkbcdn.com/s3/b/artillery.jpg"
+    style="display: inline;">
+<img data-original="//img.gkbcdn.com/s3/b/proscenic-6HykTHuh.jpg" src="//img.gkbcdn.com/s3/b/proscenic-6HykTHuh.jpg"
+    style="display: inline;">
+<img data-original="//img.gkbcdn.com/s3/b/bezior.jpg" src="//img.gkbcdn.com/s3/b/bezior.jpg"
+    style="display: inline;">
+<img data-original="//img.gkbcdn.com/s3/b/engwe.jpg" src="//img.gkbcdn.com/s3/b/engwe.jpg" style="display: inline;">
+<img data-original="//img.gkbcdn.com/s3/b/kugookirin-6TbAI6pr.jpg"
+    src="//img.gkbcdn.com/s3/b/kugookirin-6TbAI6pr.jpg" style="display: inline;">
+<img data-original="//img.gkbcdn.com/s3/b/eleglide.jpg" src="//img.gkbcdn.com/s3/b/eleglide.jpg"
+    style="display: inline;">
+    <img data-original="https://img.gkbcdn.com/s3/b/2003/mijia.jpg?v=501322930" src="https://img.gkbcdn.com/s3/b/ado.jpg"
+    style="display: inline;">
+    <img data-original="//img.gkbcdn.com/s3/b/eleglide.jpg" src="https://img.gkbcdn.com/s3/b/Logo/roborock.jpg"
+    style="display: inline;">
+    <img data-original="//img.gkbcdn.com/s3/b/eleglide.jpg" src="https://img.gkbcdn.com/s3/b/Logo/Viomi.jpg"
+    style="display: inline;">
+    <img data-original="//img.gkbcdn.com/s3/b/eleglide.jpg" src="https://img.gkbcdn.com/s3/b/artillery.jpg"
+    style="display: inline;">`
+}
+
+function giflogo() {
+    return `<img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt=""></img>
+    `
+}
+function logopng(){
+    return `<img src="https://img.gkbcdn.com/s3/b/xiaomi.jpg" alt="">
+    <img src="	https://img.gkbcdn.com/s3/b/Logo/oukitel.jpg" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/Logo/sjcam.jpg" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/kugookirin-6TbAI6pr.jpg" alt="">
+    <img src="	https://img.gkbcdn.com/s3/b/2003/mijia.jpg?v=501322930" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/ado.jpg" alt="">
+    <img src="	https://img.gkbcdn.com/s3/b/2003/mijia.jpg?v=501322930" alt="">
+    <img src="	https://img.gkbcdn.com/s3/b/Logo/himo.jpg?v=1500289693" alt="">
+    <img src="	https://img.gkbcdn.com/s3/b/Logo/himo.jpg?v=1500289693" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/2005/samebike.jpg?v=2005900933" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/2005/dyu.jpg?v=-1498692435" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/2007/segway-ninebot.jpg?v=-1662088573" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/2007/acton.jpg?v=-488455984" alt="">
+    <img src="	https://img.gkbcdn.com/s3/b/smart4u-6TQnHX7p.jpg" alt="">
+   `
+}
+function logopng2(){
+    return `<img src="https://img.gkbcdn.com/s3/b/Logo/90FUN.jpg" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/smart4u-6TQnHX7p.jpg" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/2003/mijia.jpg?v=501322930" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/smart4u-6TQnHX7p.jpg" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/Logo/himo.jpg?v=1500289693" alt="">
+    <img src="	https://img.gkbcdn.com/s3/b/2003/mijia.jpg?v=501322930" alt="">
+    <img src="	https://img.gkbcdn.com/s3/b/Logo/oukitel.jpg" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/2005/samebike.jpg?v=2005900933" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/Logo/90FUN.jpg" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/Logo/Viomi.jpg" alt="">
+    <img src="//img.gkbcdn.com/s3/b/proscenic-6HykTHuh.jpg" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/xiaomi.jpg" alt="">
+    <img src="//img.gkbcdn.com/s3/b/bezior.jpg" alt="">
+    <img src="https://img.gkbcdn.com/s3/b/Logo/90FUN.jpg" alt="">
+
+
+    `
+}
+
+export { logoimg ,giflogo,logopng,logopng2} 
+
+// <img src="https://img.gkbcdn.com/s3/b/Logo/90FUN.jpg" alt="">
+//<img src="https://img.gkbcdn.com/s3/b/happyrun.jpg" alt="">
